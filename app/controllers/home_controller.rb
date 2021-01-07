e3e60b3648a2b77b1bf685882110697ad76531af
kotawindows <tokyosato1@gmail.com>
2021-01-07 13:00:51 +0900

    api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,24 @@
 class HomeController < ApplicationController
-  def index; end
+  GOOGLE_API_KEY = ""
+
+  def find_videos(keyword, after: 1.months.ago, before: Time.now)
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = GOOGLE_API_KEY
+
+    next_page_token = nil
+    opt = {
+      q: keyword,
+      type: 'video',
+      max_results: 3,
+      order: :date,
+      page_token: next_page_token,
+      published_after: after.iso8601,
+      published_before: before.iso8601
+    }
+    service.list_searches(:snippet, opt)
+  end
+
+  def index
+    @youtube_data = find_videos('加藤純一切り抜き')
+  end
 end
