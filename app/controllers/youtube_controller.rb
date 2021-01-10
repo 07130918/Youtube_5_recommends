62d8337678ab75e78e9840b97b1bb51093091ad2
kotawindows <tokyosato1@gmail.com>
2021-01-10 14:02:03 +0900

    class variable

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,24 +1,29 @@
 class YoutubeController < ApplicationController
   GOOGLE_API_KEY = ENV['GOOGLE_APP_SECRET']
-
-  def find_videos(keyword, after: 1.months.ago, before: Time.now)
-    service = Google::Apis::YoutubeV3::YouTubeService.new
-    service.key = GOOGLE_API_KEY
-
+  @@service = Google::Apis::YoutubeV3::YouTubeService.new
+  @@service.key = GOOGLE_API_KEY
+  
+  def find_videos(keyword)
     next_page_token = nil
     opt = {
       q: keyword,
       type: 'video',
-      max_results: 1,
-      order: :date,
-      page_token: next_page_token,
-      published_after: after.iso8601,
-      published_before: before.iso8601,
+      max_results: 1
     }
-    service.list_searches(:snippet, opt)
+    @@service.list_searches(:snippet, opt)
   end
 
+  # def good_video
+  #   opt = {
+  #     # type: 'video',
+  #     max_results: 5,
+  #     myRating: 'like'
+  #   }
+  #   @service.list_videos("snippet", opt)
+  # end
+
   def index
+    # @videos = good_video
     @youtube_data = find_videos('加藤純一切り抜き集')
   end
 end
