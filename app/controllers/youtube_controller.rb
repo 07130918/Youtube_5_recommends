497150f7fb49a3c2aebeb216da41aa874854d73a
kotawindows <tokyosato1@gmail.com>
2021-01-12 12:24:51 +0900

    responseOK

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -3,13 +3,15 @@ class YoutubeController < ApplicationController
   require 'uri'
   require 'json'
   GOOGLE_API_KEY = ENV['GOOGLE_APP_SECRET']
+  CLIENT_SECRET = ENV['CLIENT_SECRET']
+
   @@service = Google::Apis::YoutubeV3::YouTubeService.new
   @@service.key = GOOGLE_API_KEY
 
   def index
-    api_responce
-    @videos = like_videos
     @youtube_data = find_videos('加藤純一')
+    api_response
+    @videos = like_videos
   end
 
   private
@@ -23,18 +25,34 @@ class YoutubeController < ApplicationController
     @@service.list_searches('snippet', option)
   end
 
-  def api_responce
-    @uri = URI('https://www.googleapis.com/youtube/v3/videos')
-    @uri.query = URI.encode_www_form({part: 'snippet', max_results: 1, my_rating: 'like', key: "#{GOOGLE_API_KEY}"})
-    @url = URI.parse("http://localhost:3000/#{@uri.query}")
-  end
-
   def like_videos
     option = {
-      max_results: 1,
+      max_results: 5,
       # my_rating: 'like',
       chart: 'most_popular',
     }
     @@service.list_videos('snippet', option)
+  end
+
+  def api_response
+    # parseでuriを区切れるようになる
+    uri = URI.parse('https://accounts.google.com/o/oauth2/token')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    req = Net::HTTP::Post.new(uri.path)
+    # urlのcode=~~の部分を取得
+    @redirect_code = request.params["code"]
+    req.set_form_data({
+      'Contant-Type' => 'application/x-www-form-urlencoded',
+      'code' => "#{@redirect_code}",
+      'client_id' => '660241016882-pl2v6ilg0k0vqi3eqdqekv6risb565pp.apps.googleusercontent.com',
+      'client_secret' => CLIENT_SECRET,
+      'redirect_uri' => 'http://localhost:3000/',
+      'grant_type'=> 'authorization_code',
+    })
+    @response = http.request(req)
+    @token = JSON.parse(@response.body)["access_token"]
+    
   end
 end
