fc128051ce77860d0aa09a0d28a10249a954f814
kotawindows <tokyosato1@gmail.com>
2021-01-15 18:46:19 +0900

    :triangular_flag_on_post: Success OAuth 2.0 myRating:like

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -34,26 +34,33 @@ class YoutubeController < ApplicationController
     @@service.list_videos('snippet', option)
   end
 
-  def get_accecc_token
+  # コードとアクセストークンを交換する関数
+  def get_access_token
+    # OAuth 2.0 の承認を突破した段階でurlのcode=~~の部分を取得
+    # @redirect_codeは後にアクセストークン、更新トークンと交換
+    # リクエスト送信に必要なため
+    response_hash = URI.decode_www_form(request.fullpath).to_h
+    @redirect_code = response_hash['/?code']
+
     # parseでuriを区切れるようになる
     uri = URI.parse('https://accounts.google.com/o/oauth2/token')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    # OAuth 2.0 の承認を突破した段階でurlのcode=~~の部分を取得
-    # @redirect_codeは後にアクセストークン、更新トークンと交換
-    # リクエスト送信に必要なため
-    response_hash = URI.decode_www_form(request.fullpath).to_h
-    @redirect_code = response_hash['/?code']
-    req_header = { 'Contant-Type' => 'application/x-www-form-urlencoded' }
-
     # POST リクエストを https://accounts.google.com/o/oauth2/token に送信
-    req = Net::HTTP::Post.new(uri.request_uri, req_header)
-    req.body =
-      "code=#{@redirect_code}&client_id=660241016882-pl2v6ilg0k0vqi3eqdqekv6risb565pp.apps.googleusercontent.com&client_secret=#{
-        CLIENT_SECRET
-      }&redirect_uri=http://localhost:3000/&grant_type=authorization_code"
+    headers = { 'Contant-Type' => 'application/x-www-form-urlencoded' }
+    params = {
+      code: "#{@redirect_code}",
+      client_id:
+        '660241016882-pl2v6ilg0k0vqi3eqdqekv6risb565pp.apps.googleusercontent.com',
+      client_secret: "#{CLIENT_SECRET}",
+      redirect_uri: "http://localhost:3000/",
+      grant_type: "authorization_code"
+    }
+    req = Net::HTTP::Post.new(uri.path)
+    req.set_form_data(params)
+    req.initialize_http_header(headers)
 
     # リクエストのレスポンスからトークン取得
     response = http.request(req)
@@ -63,29 +70,27 @@ class YoutubeController < ApplicationController
     return @access_token
   end
 
+  # アクセストークンを利用し承認されたリクエストも行える Yoyube Data Api 呼び出し関数
   def youtube_data_api
-    get_accecc_token
+    get_access_token
 
-    # GETリクエストでアクセストークンを利用する YouTube Data API の呼び出し
-    # 今やりたい事→ちゃんと条件通りにyoutubeからgetリクエストで情報をもらう
-    option = { my_rating: 'like', max_results: 1 }
-    uri =
-      URI.parse(
-        "https://www.googleapis.com/youtube/v3/videos?part=snippet&myRating=#{
-          option[:my_rating]
-        }&maxResults=#{option[:max_results]}"
-      )
+    option = {
+      my_rating: "like",
+      max_results: 1
+    }
+    uri = URI.parse("https://www.googleapis.com/youtube/v3/videos?part=snippet&maxResults=#{option[:max_results]}&myRating=#{option[:my_rating]}")
+
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
-    # https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    headers = { 'Authorization' => "Bearer #{@access_token}" }
 
-    header = { "Authorization" => "Bearer #{@access_token}"}
-
-    # request_uriは/youtube/v3/videos?part=snippet&myRating=like&maxResults=1を指す
+    # request_uriはyoutube/v3/videos?part=snippet&maxResults=#{option[:max_results]}&myRating=#{option[:my_rating]}を指す
     req = Net::HTTP::Get.new(uri.request_uri)
     req.initialize_http_header(headers)
 
-    response = https.request(req)
-    response = response.body
+    @response = https.request(req)
+    @response = @response.body.force_encoding("UTF-8")
   end
 end
