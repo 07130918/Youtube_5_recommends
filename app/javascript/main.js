b9da51086413d74eda63617dbd63c36c8b6ea81f
kotawindows <tokyosato1@gmail.com>
2021-01-18 12:40:15 +0900

    swiper

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,4 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const main = new HeroSlider(".yotube__content");
-  console.log(main);
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: false,
+    effect: "coverflow",
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
