f0f4bebb81593dcd47ad69202d45f715b943ea92
kotawindows <tokyosato1@gmail.com>
2021-01-19 11:41:43 +0900

    :recycle: scss

--- app/controllers/juntubes_controller.rb
+++ app/controllers/juntubes_controller.rb
@@ -3,7 +3,7 @@ class JuntubesController < ApplicationController
   @@service.key = GOOGLE_API_KEY
 
   def index
-    @youtube_data = find_videos('加藤純一切り抜き')
+    @youtube_search_items = youtube_search('加藤純一切り抜き').items
   end
 
   private
@@ -12,7 +12,7 @@ class JuntubesController < ApplicationController
     %w[rating relevance title videoCount viewCount].sample
   end
 
-  def find_videos(keyword)
+  def youtube_search(keyword)
     option = { q: keyword, type: 'video', max_results: 1, order: _order }
     @@service.list_searches('snippet', option)
   end
